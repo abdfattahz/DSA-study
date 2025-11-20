d3ad3243c48a83121027e18518951aa460e32ef2
abdfattahz <203197396+abdfattahz@users.noreply.github.com>
2025-11-20 21:31:52 +0800

    Finding maximum element value in the array

---
+++ arrays-basics.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+
+    int arr[] = {5, 2, 9, 1, 7};
+    int n = sizeof(arr) / sizeof(arr[0]); // say the size of the first element is 4 bytes, so if we divide it to the total size of the array (which will be 20 bytes in this case) so we will get the total amount of elements in the array
+
+    // finding the max element value of the array
+    int maxVal = arr[0]; // we assume the first element is the max value
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > maxVal) {
+            maxVal = arr[i];
+        };
+    };
+    
+    std::cout << "Max value in the array is: " << maxVal;
+    
+}

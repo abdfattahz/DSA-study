4b0df76818f6537ea223b79fc49d5ab5274441ba
abdfattahz <203197396+abdfattahz@users.noreply.github.com>
2025-11-22 03:17:25 +0800

    Finding maximum element value using vector

---
+++ vector-basics.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <iostream>
+
+int main() {
+
+    // vector push_back 
+    std::vector<int> nums = {5, 2, 9, 1, 7};
+    size_t n = nums.size();
+
+    // finding max value
+    int maxVal = nums[0];
+
+    for (size_t i = 0; i < n; i++){
+        if (nums[i] > maxVal) {
+            maxVal = nums[i];
+        }
+    };
+    
+    std::cout << "The max value is: " << maxVal;
+    std::cout << "\n";
+
+}
